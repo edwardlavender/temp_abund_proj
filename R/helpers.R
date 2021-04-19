8fe84383c7c0e7d85db211c23ef4e1094939b9a6
Edward Lavender <el72@st-andrews.ac.uk>
2021-04-19 13:43:58 +0100

    Add flexibility to plot_raster() function.

--- R/helpers.R
+++ R/helpers.R
@@ -52,11 +52,12 @@ plot_raster <- function(x,
                         zlim = NULL, 
                         gen_cols = prettyGraphics::pretty_cols_brewer,
                         profile_x = c(181, 220), profile_y = c(-90, 90),
-                        add_legend = TRUE,...){
+                        add_legend = TRUE,
+                        sp = NULL,...){
   
   paa <-  list(side = 1:4, control_axis = list(lwd.ticks = 0, labels = FALSE, lwd = 2))
   axis_args <- list(cex.axis = 1.25)
-  sp <- c(0.98, 0.99, 0.2, 0.8)
+  if(is.null(sp)) sp <- c(0.98, 0.99, 0.2, 0.8)
   if(!is.null(mask)) x <- raster::mask(x = x, mask = mask)
   if(is.null(zlim)) zlim <- raster::cellStats(x, range)
   col_param <- gen_cols(zlim,...)
@@ -64,18 +65,21 @@ plot_raster <- function(x,
     prettyGraphics::pretty_map(x, 
                                pretty_axis_args = paa,
                                add_rasters = list(x = x, smallplot = sp, axis.args = axis_args, 
-                                                  zlim = zlim, breaks = col_param$breaks, col = col_param$col), 
+                                                  zlim = zlim, breaks = col_param$breaks, col = col_param$col, 
+                                                  pretty_axis_args = list()), 
                                add_polys = list(x = land))
   } else {
     prettyGraphics::pretty_map(x, 
                                pretty_axis_args = paa,
                                add_rasters = list(x = x, plot_method = raster::image, legend = FALSE,
-                                                  zlim = zlim, breaks = col_param$breaks, col = col_param$col), 
+                                                  zlim = zlim, breaks = col_param$breaks, col = col_param$col, 
+                                                  pretty_axis_args = list()), 
                                add_polys = list(x = land))
   }
   add_profile <- TRUE
   if(add_profile){
     prettyGraphics::add_profile_lat(x = x, 
+                                    calc_lwr = NULL, calc_upr = NULL,
                                     ylim = c(-90, 90),
                                     add_fit = list(breaks = col_param$breaks, cols = col_param$col, lwd = 3),
                                     x_at = profile_x, y_at = profile_y,
